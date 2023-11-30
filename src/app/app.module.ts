796a79fb8d4957ca8bc176f079625f95652d7fcb
neutroon <191900312@ecu.edu.eg>
2023-11-30 02:10:55 +0200

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { UsersComponent } from './components/users/users.component';
+import { DeviderComponent } from './components/divider/divider.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TicketComponent,
+    ChartComponent,
+    UsersComponent,
+    DeviderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
